317a726b275438232a2a77aa9c20044ada458e6a
Sameer Sam <sameerpokharel29@gmail.com>
2021-03-03 21:06:56 +0545

    Header complete

---
+++ src/Header/Header.js
@@ -0,0 +1,37 @@
+const Header = () => {
+    return (
+        <>
+        <div className='container'>
+        <div className='row'>
+            
+            <div className='col-7 '>
+                <div className='center'>
+                <h1 className='text-center '>Hello,I am <span className='text-danger'> Samuel Pokharel</span> </h1>
+                <h2 >Front-End Developer</h2>
+                </div>
+                </div>
+        
+        <div className='col-5 '>
+        <div>
+        <img src='https://preview.colorlib.com/theme/satner/img/banner/home-right.png' 
+        alt='headerImage' className='imgStyle' />
+        </div>
+        </div>
+        
+        </div>
+            
+            
+            
+            </div> 
+      
+      
+      
+     
+            
+            
+            
+        </>
+    )
+}
+
+export default Header
